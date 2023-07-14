948d8a6cf5c44fcb9f6f37d3db7daf927a733187
Leo Gaskin <leo.gaskin@le0.gs>
2023-07-14 16:39:01 +0200

    Commit initial (broken) firefox configurations

---
+++ etc/firefox/config.js
@@ -0,0 +1,94 @@
+// IMPORTANT: Start your code on the 2nd line
+
+/* Preferences */
+
+defaultPref("security.external_protocol_requies_permission", false);
+defaultPref("browser.urlbar.decodeURLsOnCopy", true);
+defaultPref("gfx.webrender.all", false);
+defaultPref("xpinstall.signatures.required", false);
+defaultPref("network.IDN.whitelist.local", true);
+defaultPref("places.history.expiration.max_pages", 2147483647);
+defaultPref("browser.urlbar.trimURLs", false);
+defaultPref("apz.gtk.kinetic_scroll.enabled", false);
+
+/* Custom Keybindings */
+
+function mapKey(window, key, modifiers, command) {
+  let mapping = window.document.getElementById(command);
+  mapping.setAttribute("modifiers", modifiers);
+  mapping.setAttribute("key", key);
+}
+
+function setKey(window, key, modifiers, command) {
+  let mapping = window.document.createElementNS(
+    "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+    "key"
+  );
+  mapping.setAttribute("id", "key_custom_" + key + "_" + modifiers);
+  mapping.setAttribute("modifiers", modifiers);
+  mapping.setAttribute("key", key);
+  mapping.setAttribute("oncommand", command);
+  mapping.addEventListener("command", this, false);
+
+  let mainKeyset = window.document.getElementById("mainKeyset");
+  mainKeyset.appendChild(mapping);
+}
+
+function __changeKeyboardShortcuts(w) {
+  setKey(w, "W", "", "gBrowser.tabContainer.advanceSelectedTab(1, true);");
+  setKey(w, "Q", "", "gBrowser.tabContainer.advanceSelectedTab(-1, true);");
+  setKey(w, "S", "", "gBrowser.selectTabAtIndex(-1, event);");
+  setKey(w, "A", "", "gBrowser.selectTabAtIndex(0, event);");
+
+  setKey(w, "W", "shift", "gBrowser.moveTabForward();");
+  setKey(w, "Q", "shift", "gBrowser.moveTabBackward();");
+  setKey(w, "S", "shift", "gBrowser.moveTabToEnd();");
+  setKey(w, "A", "shift", "gBrowser.moveTabToStart();");
+
+  setKey(w, "X", "", "BrowserCloseTabOrWindow(event);");
+  setKey(w, "U", "", "undoCloseTab();");
+  setKey(w, "T", "", "BrowserOpenTab();");
+  let muteCommand =
+    "gBrowser.toggleMuteAudioOnMultiSelectedTabs(gBrowser.selectedTab);";
+  setKey(w, "M", "", muteCommand);
+  setKey(w, "R", "", "BrowserReload();");
+  setKey(w, "R", "shift", "BrowserReloadSkipCache();");
+
+  setKey(w, "C", "", "BrowserBack();");
+  setKey(w, "V", "", "BrowserForward();");
+}
+
+try {
+  let { classes: Cc, interfaces: Ci, manager: Cm } = Components;
+  const { Services } = Components.utils.import(
+    "resource://gre/modules/Services.jsm"
+  );
+  const { SessionStore } = Components.utils.import(
+    "resource:///modules/sessionstore/SessionStore.jsm"
+  );
+  function ConfigJS() {
+    Services.obs.addObserver(this, "chrome-document-global-created", false);
+  }
+  ConfigJS.prototype = {
+    observe: function (aSubject) {
+      aSubject.addEventListener("DOMContentLoaded", this, { once: true });
+    },
+    handleEvent: function (aEvent) {
+      let document = aEvent.originalTarget;
+      let window = document.defaultView;
+      let location = window.location;
+      if (
+        /^(chrome:(?!\/\/(global\/content\/commonDialog|browser\/content\/webext-panels)\.x?html)|about:(?!blank))/i.test(
+          location.href
+        )
+      ) {
+        if (window._gBrowser) {
+          __changeKeyboardShortcuts(window);
+        }
+      }
+    },
+  };
+  if (!Services.appinfo.inSafeMode) {
+    new ConfigJS();
+  }
+} catch (ex) {}
