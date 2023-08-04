fe6e8a0cd0ec80b7e3bc0e952cd7890cbb7c23ee
Leo Gaskin <leo.gaskin@le0.gs>
2023-08-04 02:33:08 +0200

    Extend basic surfingkeys config for sites that try to capture input
    Unfortunate that this is necessary.

--- extensions/surfingkeys.js
+++ extensions/surfingkeys.js
@@ -44,3 +44,89 @@ mapkey("y", "Capture with org-capture", function () {
     "url"
   );
 });
+
+mapkey("q", "Select previous tab", function () {
+  RUNTIME("previousTab");
+});
+mapkey("w", "Select next tab", function () {
+  RUNTIME("nextTab");
+});
+mapkey("Q", "Move current tab to left", function () {
+  RUNTIME("moveTab", {
+    step: -1,
+  });
+});
+mapkey("W", "Move current tab to right", function () {
+  RUNTIME("moveTab", {
+    step: 1,
+  });
+});
+
+mapkey("a", "Focus first tab", function () {
+  RUNTIME.repeats = 500;
+  RUNTIME("previousTab");
+});
+mapkey("s", "Focus last tab", function () {
+  RUNTIME.repeats = 500;
+  RUNTIME("nextTab");
+});
+mapkey("A", "Move current tab to far left", function () {
+  RUNTIME("moveTab", {
+    step: -500,
+  });
+});
+mapkey("S", "Move current tab to far right", function () {
+  RUNTIME("moveTab", {
+    step: 500,
+  });
+});
+
+mapkey("x", "Close current tab", function () {
+  RUNTIME("closeTab");
+});
+mapkey("r", "#4Reload the page", function () {
+  RUNTIME("reloadTab", { nocache: false });
+});
+mapkey("u", "Restore closed tab", function () {
+  RUNTIME("openLast");
+});
+mapkey("p", "pin/unpin current tab", function () {
+  RUNTIME("togglePinTab");
+});
+mapkey("m", "mute/unmute current tab", function () {
+  RUNTIME("muteTab");
+});
+
+mapkey(
+  "c",
+  "Go back in history",
+  function () {
+    history.go(-1);
+  },
+  { repeatIgnore: true }
+);
+
+mapkey(
+  "v",
+  "Go forward in history",
+  function () {
+    history.go(1);
+  },
+  { repeatIgnore: true }
+);
+
+mapkey("=", "Reset zoom level", function () {
+  RUNTIME("setZoom", {
+    zoomFactor: 0,
+  });
+});
+mapkey("+", "Zoom in", function () {
+  RUNTIME("setZoom", {
+    zoomFactor: 0.05,
+  });
+});
+mapkey("-", "Zoom out", function () {
+  RUNTIME("setZoom", {
+    zoomFactor: -0.05,
+  });
+});
